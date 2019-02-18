ff44f9bac1b39bb5c41cb38abbf7e346b0f568de
DinoLai <dinos80152@gmail.com>
2019-02-18 20:24:23 +0800

    add install shell script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ln -sf $PWD/.gitconfig $HOME/.gitconfig
+ln -sf $PWD/.zshrc $HOME/.zshrc
+ln -sf $PWD/.oh-my-zsh/oh-my-zsh.sh $HOME/.oh-my-zsh/oh-my-zsh.sh
+ln -sf $PWD/.oh-my-zsh/custom/powerlevel9k.sh $HOME/.oh-my-zsh/custom/powerlevel9k.sh
