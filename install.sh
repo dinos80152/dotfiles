0bf24e0b735c6ec9bae49e684a3d565aaf576f04
DinoLai <dinos80152@gmail.com>
2019-02-19 17:29:03 +0800

    add .profile

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 
 ln -sf $PWD/.gitconfig $HOME/.gitconfig
 ln -sf $PWD/.zshrc $HOME/.zshrc
+ln -sf $PWD/.profile $HOME/.profile
 ln -sf $PWD/.oh-my-zsh/oh-my-zsh.sh $HOME/.oh-my-zsh/oh-my-zsh.sh
 ln -sf $PWD/.oh-my-zsh/custom/powerlevel9k.sh $HOME/.oh-my-zsh/custom/powerlevel9k.sh
